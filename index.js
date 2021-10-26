618123d6e1076d36ae8333eee11d47ccba3f23fb
Wolf Laleau <devlaleau@gmail.com>
2021-10-26 18:06:37 -0400

    removing console logs and other comments

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ var API_KEY =  "563492ad6f91700001000001cd1e02693d5b4dd6a5273bcc109891fb";
 
 
 var userInput = document.querySelector("#submit");
-// var oForm = document.forms[0];
-// var name2 = oForm.elements["search"].value; 
-// console.log(name2);
-// var inputTest = document.querySelector("#input").value;
 
 userInput.addEventListener("submit", function(e) {
     e.preventDefault();
-    console.log("Enter works!!!")
-    // console.log(name2, ", userInput");
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -19,8 +13,6 @@ userInput.addEventListener("submit", function(e) {
             
             var searchedImages = document.querySelector("#image-gallery");
 
-            // var imageResults = imageResp.photos.map()
-            console.log(imageResp);
             searchedImages.innerHTML = "";
             imageResp.forEach(element => {
                 searchedImages.innerHTML += `
@@ -34,7 +26,6 @@ userInput.addEventListener("submit", function(e) {
     };
 
     var inputTest = document.querySelector("#input").value;
-    // console.log(inputTest.value, "test");
 
     xhttp.open("GET", `https://api.pexels.com/v1/search?query=${inputTest}`, true);
     xhttp.setRequestHeader("Authorization",  API_KEY);
